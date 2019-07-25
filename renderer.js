27f39b786040249d8cfcde04aca387675eff38a5
Felix O'Brien <fobcode@gmail.com>
2019-07-25 15:40:04 +1000

    fixed image on readme

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 const { ipcRenderer } = require('electron')
 getData();
-var getData = setInterval(getData, 5000);
+var getData = setInterval(getData, 1000);
 var purchases = ["cursors", "grandmas", "farms", "factories", "banks", "temples", "wizardtowers", "shipments", "alchemylabs", "portals", "timemachines", 
 "antimatters", "prisms", "chancemakers", "fractalengines"];
 
@@ -9,7 +9,7 @@ function getData(){
     
 }
 ipcRenderer.on("dataHere", function(e, data){
-    document.getElementById("cookies").innerHTML = "Cookies: " + data.cookies;
+    document.getElementById("cookies").innerHTML = "Cookies: " + data.cookies.toFixed(1);
     document.getElementById("cps").innerHTML = "Cookies Per Sec: " + data.cps;
     var html = "";
     for(var i =0; i<purchases.length; i++){
